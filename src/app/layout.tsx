6e1ad3dd1a10d803b0a92553db770a05ddd047c4
Yash Godara <yash17godara@gmail.com>
2023-09-15 03:25:19 +0530

    First Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "App",
+  description: "App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div id="__next">
+          <main>{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
