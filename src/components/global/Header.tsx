672906fe046852315e5b29e62f189e88d56ba4d6
Yash Godara <yash17godara@gmail.com>
2023-09-15 08:25:41 +0530

    Updates

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -5,7 +5,7 @@ import ThemeSwitcher from "./ThemeSwitcher";
 export const Header = () => {
   return (
     <>
-      <header className="h-15 w-full  shadow-sm dark:border-gray-700">
+      <header className="h-15 w-full shadow-sm dark:border-gray-700 dark:shadow-[#424242]">
         <div className="container  px-4 sm:px-6 py-4 flex justify-between items-center">
           {/* Logo */}
           <h2 className="font-bold text-3xl  text-gray-900 dark:text-white">
